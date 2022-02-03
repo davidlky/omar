6aaf54dfa022083aa2725ebe0fc4c86cdf138c9e
David Liu <david.liu@cognite.com>
2022-02-03 14:03:06 +0100

    add amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDoc } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyABxIJdFWIMhAvZozs9ty9Sq0MXDwTshPk',
+  authDomain: 'platypus-demo.firebaseapp.com',
+  projectId: 'platypus-demo',
+  storageBucket: 'platypus-demo.appspot.com',
+  messagingSenderId: '50858006602',
+  appId: '1:50858006602:web:bfefeaa688133898a8aeb3',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Learn React v2
         </a>
       </header>
     </div>
